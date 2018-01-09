9ad9ba0d4a26b7c2e50954868b5e03caaa57cb73
CostiAgapi <agapi.constantin92@gmail.com>
2018-01-09 10:12:35 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+  app = express(),
+  port = 8000;
+  bodyParser = require('body-parser');
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  // app.use(bodyParser.json());
+  var routes = require('./api/routes/routes');
+  routes(app);
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
