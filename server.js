a566aaa9891e32e372ea6542398c50647081cf13
CostiAgapi <agapi.constantin92@gmail.com>
2018-01-10 10:42:36 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require('express'),
   app = express(),
   port = 8000;
   bodyParser = require('body-parser');
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(bodyParser.json());
+  sqlite3 = require('sqlite3').verbose();
+  dbutils = require('./api/utils/dbutils');
+
+  app.on('listening', function () {
+    dbutils.createDb();
+  });
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
   var routes = require('./api/routes/routes');
   routes(app);
 
